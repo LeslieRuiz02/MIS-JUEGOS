67105d675c9a997954ac776dfe6d06a1a5956c09
Leslie Ruiz <ruizmurilloleslieelizabeth@gmail.com>
2026-03-17 13:20:47 -0600

    feat: Implement a basic web page with a clickable button and styling.ESTA FUE MI PRIMERA PRUEBA EN ESPAÑOL

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const miBoton = document.getElementById('miBoton');
+
+    miBoton.addEventListener('click', () => {
+        alert('¡Bienvenido! Has hecho clic en el botón. Ya estamos listos para practicar Git.');
+    });
+});
